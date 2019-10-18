1f22f7c852d581265a6e2b7e7a0185445d4f50a0
forgong <qeqe3939@naver.com>
2019-10-18 12:54:14 +0900

    project

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const indexRouter = require('./routes/index');
 const gameRouter = require('./routes/game');
 
 app.use(express.static(__dirname + '/public'));
+
 app.use('/', indexRouter);
 app.use('/game', gameRouter);
-
-
 
 app.listen(3000, function(){
     console.log('Server start: port 3000');
--- routes/index.js
+++ routes/index.js
@@ -4,37 +4,40 @@ const app = express();
 const db = require('../utils/db');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false}));
+router.use(bodyParser.json());
 
-router.post('/', function (req, res) {
+router.get('/', function (req, res) {
     res.sendFile('../public/index.html');
 })
 
 router.post('/Login/Signup.html', function(req, res){
-    let {id, password, email} = req.body;
-    db.query(`insert into user (id, password, email) values (?, ?, ?)`, [id, password, email], function(err){
-        if(err){
-            console.log(err);
-            throw err;
-        }
+    let userId = req.body['id'];
+    let userPw = req.body['password'];
+    let userEmail = req.body['email'];
+    let userAddress = req.body['address'];
+    console.log(userId, userPw, userEmail, userAddress);
+    db.query(`insert into user (id, password, email, address) values (?, ?, ?, ?)`, [userId, userPw, userEmail, userAddress], function(err){
+        
         res.redirect('/Login/Login_popup.html');
     });
 });
 
 router.post('/Login_popup.html', function(req, res){
-    let {id, password} = req.body;
-    console.log(id,password);
-    db.query(`select * from user where user.id=? `, [id], function(err, userinfo){
-        // if(err){
-            
-        // }
-        // if (!userinfo.length){
-        // }
-        // if(userinfo[0].password != password){
-        //         alert("비밀번호가 틀립니다.");
-        //     }
-        if(userinfo[0].password === password){
+    let userId = req.body['id'];
+    let userPw = req.body['password'];
+    console.log(userId,userPw);
+    db.query(`select * from user where user.id=? `, [userId], function(err, userinfo){
+        if(err){
+         throw err;    
+        }
+        if(userinfo[0] == undefined){
+            res.send('아이디가 없습니다.')
+        }
+        if(userinfo[0].password != userPw){
+            res.send('비밀번호 오류');
+        }
+        if(userinfo[0].password === userPw){
             
             res.redirect('/');
         }
