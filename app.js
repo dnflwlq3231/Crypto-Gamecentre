07788cd5b7f6f63a1be8ed7d63948fce800ea6ee
curunie <lnhog403@gmail.com>
2019-11-11 14:08:25 +0900

    add console-stamp module

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const sessionParser = require('express-session');
 const mysqlStroe = require('express-mysql-session')(sessionParser);
 const dbOption = require('./config/db.json')
 
+require('console-stamp')(console, 'yyyy/mm/dd HH:MM:ss.l');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
